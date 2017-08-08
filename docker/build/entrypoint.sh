30e0e28c142bba12376f4adf0c793a74e83bd950
Omar Padron <omar.padron@kitware.com>
2017-08-08 14:30:30 -0400

    nearly complete docker setup

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -2,6 +2,27 @@
 
 cd /girder
 pip install -e '.[plugins]'
-girder-install web --all-plugins
-exec girder-install web "$@"
+
+if [ "$1" '=' '--post-install' ] ; then
+    shift
+
+    pip install girder-client
+    echo "RUNNING POST INSTALL"
+    python /girder-post-install.py \
+        --host "$GIRDER_HOST" \
+        --admin "$GIRDER_ADMIN" \
+        --gridfs-db-name "$GIRDER_GRIDFS_DB_NAME" \
+        --user "$GIRDER_USER" \
+        --broker "$GIRDER_BROKER"
+    echo "POST INSTALL COMPLETE"
+
+    girder-install web --all-plugins
+
+    echo "SLEEPING"
+    while true ; do
+        sleep 600
+    done
+else
+    exec girder-install web "$@"
+fi
 
